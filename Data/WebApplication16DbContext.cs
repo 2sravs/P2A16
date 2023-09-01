1ebb4ed3ebfcc20652db26acc281f7a207371882
2Sravs <sravani.chekuri1243#gmail.com>
2023-09-01 16:26:19 +0530

    firstcommit

---
+++ Data/WebApplication16DbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebApplication16.Models;
+
+namespace WebApplication16.Data
+{
+    public class WebApplication16DbContext : DbContext
+    {
+        public WebApplication16DbContext (DbContextOptions<WebApplication16DbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebApplication16.Models.Task> Task { get; set; } = default!;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using WebApplication16.Data;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<WebApplication16DbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("WebApplication16DbContext") ?? throw new InvalidOperationException("Connection string 'WebApplication16DbContext' not found.")));
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Tasks}/{action=Index}/{id?}");
+
+app.Run();
